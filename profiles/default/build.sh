d3693d210d6b05111317162653b9c325b417fae1
Tomoatsu Shimada <shimada@walbrix.com>
2021-05-18 15:52:00 +0900

    transient mode added wait for display to be connected when start ui first time from last boot

--- profiles/default/build.sh
+++ profiles/default/build.sh
@@ -8,53 +8,6 @@ build-kernel
 
 emerge -uDN -bk --binpkg-respect-use=y --exclude='sys-kernel/*' world @common @walbrix @rescue
 
-mkdir -p /tmp/initramfs
-
-cat << EOS > /tmp/initramfs.lst
-bin/cat
-bin/cp
-bin/mv
-bin/sed
-bin/umount
-lib64/libacl.so.1
-lib64/libattr.so.1
-lib64/libc.so.6
-lib64/ld-linux-x86-64.so.2
-lib64/libblkid.so.1
-lib64/libuuid.so.1
-lib64/libmount.so.1
-lib64/librt.so.1
-lib64/libpthread.so.0
-lib64/liblzo2.so.2
-lib64/libsmartcols.so.1
-lib64/libz.so.1
-lib64/libdevmapper-event.so.1.02
-lib64/libdl.so.2
-lib64/libdevmapper.so.1.02
-lib64/libaio.so.1
-lib64/libm.so.6
-lib64/libsystemd.so.0
-lib64/libudev.so.1
-lib64/libcap.so.2
-sbin/btrfs
-sbin/switch_root
-sbin/mkfs.btrfs
-sbin/mkswap
-sbin/swapon
-sbin/vgchange
-usr/lib64/libiniparser4.so.1
-usr/lib64/libzstd.so.1
-usr/lib64/liblz4.so.1
-usr/lib64/libgcrypt.so.20
-usr/lib64/libgpg-error.so.0
-usr/sbin/fsck.fat
-EOS
-
-mkdir -p /tmp/initramfs/usr/lib64
-cp -L `gcc -print-file-name=libgcc_s.so.1` /tmp/initramfs/usr/lib64/
-cp -L `gcc -print-file-name=libstdc++.so.6` /tmp/initramfs/usr/lib64/
-g++ -std=c++2a -lblkid -lmount -liniparser4 /init.cpp /initlib.cpp /fat.cpp -o /tmp/initramfs/init
-tar cf - -C / -T /tmp/initramfs.lst -h | tar xvf - -C /tmp/initramfs
+g++ -std=c++2a -static-libgcc -static-libstdc++ -o /init /init.cpp /init-overrides.cpp -lmount -lblkid -liniparser4
 rm -f /boot/initramfs
-cd /tmp/initramfs && find . | cpio -H newc -o > /boot/initramfs
-
+/init > /boot/initramfs
--- profiles/default/init-overrides.cpp
+++ profiles/default/init-overrides.cpp
@@ -1,186 +1,7 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/mount.h>
-#include <sys/utsname.h>
-
 #include <iostream>
-#include <fstream>
-#include <regex>
+#include "init.h"
 
-#include "initlib.h"
-#include "fat.h"
-
-class MyInit : public Init {
-  bool is_installer();
-  bool is_rescue();
-protected:
-  virtual void mount_boot(const Partition& boot_partition, const std::filesystem::path& mountpoint);
-  virtual void mount_rw(const std::filesystem::path& boot, const std::filesystem::path& mountpoint);
-  virtual bool activate_swap(const std::filesystem::path& boot);
-  virtual std::pair<std::string,int> get_default_network_interface_name();
-};
-
-static bool check_target(const char* target_name)
-{
-  std::ifstream cmdline("/proc/cmdline");
-  while (!cmdline.eof()) {
-    std::string arg;
-    cmdline >> arg;
-    if (arg == std::string("systemd.unit=") + target_name) return true;
-  }
-  return false;
-}
-
-bool MyInit::is_installer()
-{
-  return is_boot_partition_readonly() || check_target("installer.target");
-}
-
-bool MyInit::is_rescue()
-{
-  return is_boot_partition_readonly() || check_target("transient.target");
-}
-
-void MyInit::mount_boot(const Partition& boot_partition,
-  const std::filesystem::path& mountpoint)
-{
-  if (is_installer()) {
-    std::cout << "Loading..." << std::endl;
-    auto temp_ro_mount = std::filesystem::path(mountpoint).replace_filename("cdrom");
-    std::filesystem::create_directory(temp_ro_mount);
-    if (mount(boot_partition.path, temp_ro_mount, "auto", MS_RDONLY) != 0)
-      RUNTIME_ERROR("mount /mnt/cdrom");
-    if (mount("tmpfs", mountpoint, "tmpfs", MS_NODEV|MS_NOSUID|MS_STRICTATIME, "mode=755") != 0)
-      RUNTIME_ERROR("mount tmpfs on /mnt/boot");
-    //else
-    std::filesystem::copy_file(temp_ro_mount / "system.img", mountpoint / "system.img");
-    auto bootloader = temp_ro_mount / "efi/boot/bootx64.efi";
-    if (is_file(bootloader)) {
-      auto bootloader_dir = mountpoint / "efi/boot";
-      std::filesystem::create_directories(bootloader_dir);
-      std::filesystem::copy_file(bootloader, bootloader_dir / "bootx64.efi");
-    }
-    auto ini_file = temp_ro_mount / "system.ini";
-    if (is_file(ini_file)) {
-      std::filesystem::copy_file(ini_file, mountpoint / "system.ini");
-    }
-    auto openvpn = temp_ro_mount / "openvpn";
-    if (is_dir(openvpn)) {
-      cp_a(openvpn, mountpoint);
-    }
-    umount(temp_ro_mount);
-  } else {
-    bool do_mkfs = false;
-    bool do_mkswap = false;
-    auto fd = open(boot_partition.path.c_str(), O_RDWR);
-    if (fd >= 0) {
-      // create data files on the FAT filesystem in fastest way
-      fat_mount(fd);
-      FatFileCreateResult rst = fat_file_create("system.dat", 4294967294L);
-      if (rst == FatFileCreateResult::NOSPACE) {
-        rst = fat_file_create("system.dat", 3221225472L);
-        if (rst == FatFileCreateResult::NOSPACE) {
-          rst = fat_file_create("system.dat", 2147483648L);
-          if (rst == FatFileCreateResult::NOSPACE) {
-            rst = fat_file_create("system.dat", 1073741824L);
-          }
-        }
-      }
-      if (rst == FatFileCreateResult::OK) do_mkfs = true;
-
-      rst = fat_file_create("system.swp", 1073741824L);
-      if (rst == FatFileCreateResult::NOSPACE) {
-        rst = fat_file_create("system.swp", 536870912L);
-        if (rst == FatFileCreateResult::NOSPACE) {
-          rst = fat_file_create("system.swp", 268435456L);
-        }
-      }
-      if (rst == FatFileCreateResult::OK) do_mkswap = true;
-      fat_umount();
-      close(fd);
-    }
-    if (mount(boot_partition.path, mountpoint, "vfat", MS_RELATIME, "fmask=177,dmask=077") != 0) {
-      std::cout << "Boot partition filesystem corrupted. Attempting repair..." << std::endl;
-      repair_fat(boot_partition.path);
-      if (mount(boot_partition.path, mountpoint, "vfat", MS_RELATIME, "fmask=177,dmask=077") != 0) {
-        RUNTIME_ERROR("mount boot partition");
-      }
-    }
-    if (do_mkfs) {
-      if (mkfs_btrfs(mountpoint / "system.dat") == 0) {
-        std::cout << "RW layer created." << std::endl;
-      }
-    }
-    if (do_mkswap) {
-      if (mkswap(mountpoint / "system.swp") == 0) {
-        std::cout << "Swap file created." << std::endl;
-      }
-    }
-    //std::filesystem::create_directories(mountpoint / "vm");
-  }
-}
-
-void MyInit::mount_rw(const std::filesystem::path& boot,
-  const std::filesystem::path& mountpoint)
-{
-  if (is_installer() || is_rescue()) {
-    mount_transient_rw_layer(mountpoint);
-    return;
-  }
-  //else
-
-  auto datafile = boot / "system.dat";
-
-  if (!std::filesystem::exists(datafile) && get_free_disk_space(boot) >= 1024L*1024*1024*2 ) {
-    std::cout << "RW layer does not exist. Creating..." << std::flush;
-    if (create_btrfs_imagefile(datafile, 128*1024*1024) == 0) {
-      std::cout << "done." << std::endl;
-    } else {
-      std::cout << "failed." << std::endl;
-    }
-  }
-
-  std::cout << "Mounting RW layer..." << std::endl;
-  //enable_lvm();
-  btrfs_scan();
-  bool rw_layer_mounted = mount_loop(datafile, mountpoint, "btrfs", MS_RELATIME, "compress=zstd") == 0;
-  if (!rw_layer_mounted) {
-    std::cout << "Failed to mount RW layer. Attempting repair." << std::endl;
-    repair_btrfs(datafile);
-    rw_layer_mounted = mount_loop(datafile, mountpoint, "btrfs", MS_RELATIME, "compress=zstd") == 0;
-  }
-  if (!rw_layer_mounted) {
-    std::cout << "No valid persistent RW layer. Falling back to tmpfs." << std::endl;
-    mount_transient_rw_layer(mountpoint);
-  }
-}
-
-bool MyInit::activate_swap(const std::filesystem::path& boot)
-{
-  if (is_installer()) return false;
-  //else
-  auto swapfile = boot / "system.swp";
-
-  if (!exists(swapfile) && get_free_disk_space(boot) >= 1024L*1024*1024*2 ) {
-    std::cout << "Swapfile does not exist. Creating..." << std::flush;
-    if (create_swapfile(swapfile, 1024L*1024*1024) == 0) {
-      std::cout << "done." << std::endl;
-    } else {
-      std::cout << "failed." << std::endl;
-    }
-  }
-
-  if (!is_file(swapfile)) return false;
-  std::cout << "Activating swap..." << std::endl;
-  return (swapon(swapfile) == 0);
-}
-
-std::pair<std::string,int> MyInit::get_default_network_interface_name()
-{
-  return std::make_pair("xenbr0", 52);
-}
-
-std::filesystem::path init()
+void init::hooks::print_banner()
 {
   std::cout
     << R"( __      __        .__ ___.         .__        )" "\n"
@@ -189,47 +10,25 @@ std::filesystem::path init()
     << R"( \        /  / __ \|  |_| \_\ \  | \/  |>    < )" "\n"
     << R"(  \__/\  /  (____  /____/___  /__|  |__/__/\_ \)" "\n"
     << R"(       \/        \/         \/               \/)" << std::endl;
-/* TODO: remove after -
-  utsname buf;
-  if (uname(&buf) == 0) {
-    std::cout << buf.release << std::endl;
-  }
-*/
-  MyInit init;
-  init.setup();
-  auto newroot = init.get_newroot();
-  const auto vm_root = newroot / "var/vm";
-
-  try {
-    const auto adhoc_vol_src = newroot / "run/initramfs/boot/vm";
-    if (is_dir(adhoc_vol_src)) {
-      const auto adhoc_vol = vm_root / "@adhoc";
-      std::filesystem::create_directories(adhoc_vol);
-      bind_mount(adhoc_vol_src, adhoc_vol);
-    }
-
-    const char *boot_partition_uuid = getenv("boot_partition_uuid");
-    if (boot_partition_uuid) {
-      const auto default_vol = vm_root / "@default";
-      std::filesystem::create_directories(default_vol);
-      const auto wbdata = std::string("LABEL=") + "wbdata-" + boot_partition_uuid;
-      const auto btrfs_options = std::string("compress=") + init.ini_string("data-volume-compression", "no") +
-        (init.ini_bool("data-volume-cow", false) ? "" : ",nodatacow") +
-        (init.ini_bool("data-volume-trim", false) ? ",discard" : "") +
-        ",degraded";
-      if (mount(wbdata, default_vol, "btrfs", MS_RELATIME, btrfs_options) != 0) {
-        std::cout << "Default data partition couldn't be mounted." << std::endl;
-      }
-    }
-  }
-  catch (const std::exception& ex) {
-    std::cout << "Exception occured when mounting data volumes. " << ex.what() << std::endl;
-  }
-  return newroot;
 }
 
-void shutdown()
+void init::hooks::setup_data_subvolumes(const std::filesystem::path& mnt_path) 
 {
-  Shutdown shutdown;
-  shutdown.cleanup();
+	const auto data_partition_dev_path = mnt_path / "vm";
+	if (!init::lib::is_dir(data_partition_dev_path) && init::lib::exec(init::progs::BTRFS, {"subvolume", "create", data_partition_dev_path}) != 0) {
+		std::cout << "Failed to create vm data subvolume under data partition." << std::endl;
+	}
 }
+
+void init::hooks::post_init(const std::filesystem::path& newroot,
+  std::optional<std::tuple<std::filesystem::path,std::optional<std::string/*uuid*/>,std::optional<std::string/*fstype*/>>> data_partition,
+  inifile_t)
+{
+	if (!data_partition) return;
+	const auto data_partition_dev_path = std::get<0>(data_partition.value());
+	const auto vm_default = newroot / "var/vm/@default";
+	if (!init::lib::is_dir(vm_default) || init::lib::mount(data_partition_dev_path, vm_default, "btrfs", MS_RELATIME, "subvol=vm") != 0) {
+		std::cout << "Default VM subvolume couldn't be mounted." << std::endl;
+	}
+}
+
