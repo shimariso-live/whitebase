0639ac5e931db0095ba751bbe102a166ca74109b
Tomoatsu Shimada <shimada@walbrix.com>
2021-05-20 13:02:08 +0900

    rescue artifact removed as transient mode can be used for that purpose

--- profiles/default/build.sh
+++ profiles/default/build.sh
@@ -6,7 +6,7 @@ popd
 
 build-kernel
 
-emerge -uDN -bk --binpkg-respect-use=y --exclude='sys-kernel/*' world @common @walbrix @rescue
+emerge -uDN -bk --binpkg-respect-use=y --exclude='sys-kernel/*' world @common @walbrix
 
 g++ -std=c++2a -static-libgcc -static-libstdc++ -o /init /init.cpp /init-overrides.cpp -lmount -lblkid -liniparser4
 rm -f /boot/initramfs
