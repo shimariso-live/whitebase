7c2d9e11300fcca552f2b6351a175409b14d9e3a
Tomoatsu Shimada <shimada@walbrix.com>
2021-05-22 14:50:41 +0900

    genpake-install compliant

--- profiles/default/build.sh
+++ profiles/default/build.sh
@@ -10,7 +10,7 @@ if ! equery -q l xen-tools && ! equery -q l qemu; then
 	USE="-system-qemu -ipxe -pam -pygrub -python -qemu-traditional -rombios" emerge -1 -k xen-tools
 fi
 
-emerge -uDN -bk --binpkg-respect-use=y --exclude='sys-kernel/*' world @all
+emerge -uDN -bk --binpkg-respect-use=y --exclude='sys-kernel/*' world @all @genpack-install
 
 g++ -std=c++2a -static-libgcc -static-libstdc++ -o /init /init.cpp /init-overrides.cpp -lmount -lblkid -liniparser4
 rm -f /boot/initramfs
