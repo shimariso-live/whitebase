6a47ed787a54282d2b882fcea41afa4e649bcaee
Tomoatsu Shimada <shimada@walbrix.com>
2021-09-26 19:22:21 +0900

    script for cd image creation

---
+++ create-cd-image.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+SUDO=
+USERID=`id -u`
+GROUPID=`id -g`
+if [[ "$USERID" -ne 0 ]]; then
+	SUDO=sudo
+fi
+$SUDO genpack-install --disk=walbrix.iso --cdrom --label=WBINSTALL --system-cfg=artifacts/whitebase/boot/grub/installer.cfg whitebase-x86_64.squashfs
+[ -n "$SUDO" ] && $SUDO chown $USERID.$GROUPID walbrix.iso
+xorriso -as mkisofs -f -J -r -V WBSOURCE -o walbrix-source.iso work/x86_64/profiles/*/cache/distfiles -graft-points profiles=profiles
+
