46f5d80fd8b6cdfeec676953674bdba3640252e4
Tomoatsu Shimada <shimada@walbrix.com>
2021-09-26 19:33:46 +0900

    include portage tree too

--- create-cd-image.sh
+++ create-cd-image.sh
@@ -7,5 +7,6 @@ if [[ "$USERID" -ne 0 ]]; then
 fi
 $SUDO genpack-install --disk=walbrix.iso --cdrom --label=WBINSTALL --system-cfg=artifacts/whitebase/boot/grub/installer.cfg whitebase-x86_64.squashfs
 [ -n "$SUDO" ] && $SUDO chown $USERID.$GROUPID walbrix.iso
-xorriso -as mkisofs -f -J -r -V WBSOURCE -o walbrix-source.iso work/x86_64/profiles/*/cache/distfiles -graft-points profiles=profiles
+xorriso -as mkisofs -f -J -r -V WBSOURCE -o walbrix-source.iso work/x86_64/profiles/*/cache/distfiles -graft-points profiles=profiles portage=work/portage
 
+# qemu-system-x86_64 --enable-kvm -cdrom walbrix.iso -m 1024 -hda disk.img -monitor stdio -no-shutdown
